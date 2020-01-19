c620b6e86f20aa5759c7922172c0a8dc6e886bb5
DMKalchev18 <DMKalchev18@codingburgas.bg>
2020-01-19 22:37:24 +0200

    Text formating

--- js/site.js
+++ js/site.js
@@ -1,14 +1,14 @@
 
 
-function initScenes(){
+function initScenes() {
 
-   // build scene
-   new ScrollMagic.Scene({
+  // build scene
+  new ScrollMagic.Scene({
     triggerElement: "#trigger1",
     triggerHook: 0.9, // show, when scrolled 10% into view
     duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
     offset: 50 // move trigger to center of element
-})
+  })
     .setClassToggle("#reveal1", "visible") // add class to reveal
     .addTo(controller);
 
@@ -18,7 +18,7 @@ function initScenes(){
     triggerHook: 0.9, // show, when scrolled 10% into view
     duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
     offset: 50 // move trigger to center of element
-})
+  })
     .setClassToggle("#reveal2", "visible") // add class to reveal
     .addTo(controller);
 
@@ -30,10 +30,8 @@ $(function () {
 
   let paraTemp = $("#vTemp");
 
-  //paraTemp.text("Hello Jelyazko!");
-
   $.getJSON("http://data.sensor.community/airrohr/v1/sensor/38303/", function (data) {
-    
+
     console.log(data);
 
     let temp1 = parseFloat(data[0].sensordatavalues[0].value);
