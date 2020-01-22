2eb116b1fcd218d787eabf30de5c014aff330096
DMKalchev18 <DMKalchev18@codingburgas.bg>
2020-01-22 09:34:19 +0200

    add animation to the graphs

--- js/site.js
+++ js/site.js
@@ -20,28 +20,18 @@ function initScenes() {
     .setClassToggle("#reveal2", "visible") // add class to reveal
     .addTo(controller);
 
+ // build scene
+  new ScrollMagic.Scene({
+    triggerElement: "#trigger3",
+    triggerHook: 1, // show, when scrolled 10% into view
+    duration: "117%", // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 50 // move trigger to center of element
+  })
+    .setClassToggle("#reveal3", "visible") // add class to reveal
+    .addTo(controller);
 }
 
-$(document).ready(function(){
-  //Take your div into one js variable
-  var div = $("#divToShowHide");
-  //Take the current position (vertical position from top) of your div in the variable
-  var pos = div.position();
-  //Now when scroll event trigger do following
-  $(window).scroll(function () {
-   var windowpos = $(window).scrollTop();
-   //Now if you scroll more than 100 pixels vertically change the class to AfterScroll
-   // I am taking 100px scroll, you can take whatever you need
-   if (windowpos >= (pos.top - 1500)) {
-     div.addClass("AfterScroll");
-   }
-   //If scroll is less than 100px, remove the class AfterScroll so that your content will be hidden again 
-   else {
-     s.removeClass("AfterScroll");
-   }
-   //Note: If you want the content should be shown always once you scroll and do not want to hide it again when go to top agian, no need to write the else part
- });
-});
+
 
 $(function () {
 
@@ -97,6 +87,5 @@ $(function () {
   });
 
 });
-
 
 
